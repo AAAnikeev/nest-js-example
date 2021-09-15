4305e082d4aaf4a8e563ab6bd72730744e658917
Alexandr <cahcahbi432@yandex.ru>
2021-09-15 16:56:44 +0300

    Добавлена работа по https, в gitignore добавлены маски сертификатов и ключей.

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
+import { readFileSync } from 'fs';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  //Add files with sertificates
+  const httpsOptions = {
+    key: readFileSync('./server.key'),
+    cert: readFileSync('./server.crt'),
+  }
+  const app = await NestFactory.create(AppModule, {httpsOptions,});
+
+  // Or use http
+  //const app = await NestFactory.create(AppModule);
   await app.listen(3000);
 }
 bootstrap();
